e28c35bdf0a952d0c52160a34ac1d22d795802d1
Nwabisa Redwood-Sawyerr <83427418+nwabimady@users.noreply.github.com>
2023-12-06 15:41:17 +0000

    Array/Object spreading/Object descruturing

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const number = 5
-let result
+const objA = {key1: "Value 1", key2: "Value 2"}
+const objB = {key3: "Value 3", key4: "Value 4"}
 
-if (number > 10) {
-    result = number * 2
-} else {
-    result = number * 5
+const objC = {...objA, ...objB, key2: "updated value 2"}
+
+//console.log(objA, objB, objC)
+
+const arrA = [1,2,3]
+const arrB = [4,5,6]
+
+const arrC = [...arrA, ...arrB, 7,8,9 ]
+
+//console.log(arrC)
+
+const user = {
+    name: "Nwabisa",
+    lastName: "Redwood-Sawyerr",
+    email: "nwabimady@gmail.com",
+    age: 50,
+    job: "Architect",
+    programmingLanguages: ["Javascript","Typescript"],
+    country: "Isle of Man",
+    nationality: "South African",
+    websites: "http://nwabimady.wix.com/nwabisa-madyibi#!projects/cwx6"
 }
 
-const newValue = result >= 30 ? 1 : 2
+const { name, lastName, age, ...otherInfo } = user
 
-console.log(newValue)
+//console.log(user, `Name: ${name}`, `Last Name: ${lastName}`, `Age: ${age}`, otherInfo)
+
+const myArray = [1,2,3]
+const [firstValue, secondValue, thirdValue] = myArray
+console.log(`First Value: ${firstValue}, Second Value: ${secondValue}, Third Value: ${thirdValue}`)
